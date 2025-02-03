7ba41f3fc51d8f2b96e57599b8026ca21b2e0ff1
Mogee0 <141176728+Mogee0@users.noreply.github.com>
2025-02-03 16:04:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
+
+function App() {
+    return (
+        <div>
+            {/* Your website content */}
+            <Analytics />
+        </div>
+    );
+}
+
+export default App;
